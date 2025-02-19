804bbdce10206ecfd47fa85692bed672ac8c4490
Philipp Kolbe <philipp.kolbe@student.hpi.de>
2025-02-19 16:39:16 +0100

    make all

--- tests/test_logger_decorator.py
+++ tests/test_logger_decorator.py
@@ -1,8 +1,10 @@
-from unittest.mock import MagicMock
 import sys
+from unittest.mock import MagicMock
 
 from lightning.pytorch.loggers.logger import Logger
+
 from neural_bandits.benchmark.logger_decorator import OnlineBanditLoggerDecorator
+
 
 def test_online_bandit_logger_decorator_basic() -> None:
     mock_logger = MagicMock(spec=Logger)
@@ -17,8 +19,12 @@ def test_online_bandit_logger_decorator_basic() -> None:
     # Decorator should flush and write to stdout once
     sys.stdout.flush()
     # Ensure that the wrapped logger’s log_metrics was called with updated metrics
-    mock_logger.log_metrics.assert_called_with({"training_run": 0, "loss": 0.5, "pre_metric": 123.0}, 1)
-    assert decorator.pre_training_metrics is None  # pre_metrics cleared after first usage
+    mock_logger.log_metrics.assert_called_with(
+        {"training_run": 0, "loss": 0.5, "pre_metric": 123.0}, 1
+    )
+    assert (
+        decorator.pre_training_metrics is None
+    )  # pre_metrics cleared after first usage
 
     # 3. Check finalize increments training_run
     decorator.finalize(status="finished")
