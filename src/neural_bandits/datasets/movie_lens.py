0a84eaa77a49059facdab2240e64a5c179b8bdd6
Robert Weeke <Robert.Weeke@student.hpi.uni-potsdam.de>
2025-02-20 01:33:02 +0100

    add more descriptive class comment with references

--- src/neural_bandits/datasets/movie_lens.py
+++ src/neural_bandits/datasets/movie_lens.py
@@ -196,8 +196,12 @@ def _setup_movielens(
 
 
 class MovieLensDataset(AbstractDataset[torch.Tensor]):
-    """MovieLens dataset for combinatorial contextual bandits. We build the context by using the SVD decomposition of the
-    user-movie matrix. The context is the outer product of the user and movie features.
+    """MovieLens dataset for combinatorial contextual bandits. The dataset is provided by the GroupLens Research
+    specifically by Harper and Konstan (2015, The MovieLens Datasets: History and Context). More information can be
+    found [here](https://www.grouplens.org/datasets/movielens/).
+    We build the context by using the SVD decomposition of the user-movie matrix. The context is the outer product of
+    the user and movie features. This approach is described in "A contextual-bandit approach to personalized news
+    article recommendation" by Li et. al. (2010).
 
     Args:
         dest_path: The directory where the dataset is / will be stored.
