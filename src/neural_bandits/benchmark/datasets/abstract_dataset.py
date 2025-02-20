b68187716438de6f2bfceeb36c56dd512c722b24
Philipp Kolbe <philipp.kolbe@student.hpi.de>
2025-02-20 19:44:29 +0100

    input size in abstract dataset

--- src/neural_bandits/benchmark/datasets/abstract_dataset.py
+++ src/neural_bandits/benchmark/datasets/abstract_dataset.py
@@ -12,21 +12,26 @@ class AbstractDataset(ABC, Dataset[Tuple[torch.Tensor, torch.Tensor]]):
     Abstract class for a dataset that is derived from PyTorch's Dataset class.
     Additionally, it provides a reward method for the specific bandit setting.
 
-    Subclasses should have the following to attributes:
+    Subclasses should have the following attributes:
     - num_actions  - The maximum number of actions available to the agent.
     - context_size - The standard size of the context vector.
         If needs_disjoint_contextualization is True, the context size will be multiplied by the number of actions.
+    - input_size   - The size of the input vector for the neural network.
+        If needs_disjoint_contextualization is True, the input size will be context_size * num_actions. Otherwise context_size.
     """
 
     num_actions: int
     context_size: int
+    input_size: int
 
     def __init__(self, needs_disjoint_contextualization: bool = False) -> None:
         self.contextualizer: MultiClassContextualizer | Callable[[Any], Any]
         if needs_disjoint_contextualization:
             self.contextualizer = MultiClassContextualizer(self.num_actions)
+            self.input_size = self.context_size * self.num_actions
         else:
             self.contextualizer = lambda x: x
+            self.input_size = self.context_size
 
     @abstractmethod
     def __len__(self) -> int:
