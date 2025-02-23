37f1df2fa654efbf5ff93967e2c8e70737c8fd64
Robert Weeke <Robert.Weeke@student.hpi.uni-potsdam.de>
2025-02-23 19:22:34 +0100

    fix bad test `test_neural_linear_bandit_forward_small_sample_correct`

--- tests/test_neural_linear.py
+++ tests/test_neural_linear.py
@@ -106,15 +106,16 @@ def test_neural_linear_bandit_forward_small_sample_correct() -> None:
     output, _ = bandit(contextualized_actions)
     assert output.shape == (1, 2)
     # assert that the correct action is selected
-    assert torch.all(output == torch.tensor([[1, 0]]))
+    assert torch.all(output == torch.tensor([[0, 1]]))
 
     # now change the weights of the head to only regard the second feature (x2)
     bandit.theta = torch.tensor([0.0, 1.0])
+    bandit.precision_matrix = torch.tensor([[0.01, 0.0], [0.0, 0.01]])
 
     output, _ = bandit(contextualized_actions)
     assert output.shape == (1, 2)
     # assert that the correct action is selected
-    assert torch.all(output == torch.tensor([[0, 1]]))
+    assert torch.all(output == torch.tensor([[1, 0]]))
 
     # TODO: test output probabilities are correct
 
